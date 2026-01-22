8a3621e21fd6b2f2d74c238c5852ffdaf6bd9d31
Nabalzam <nabhan.alzam@gmail.com>
2026-01-22 21:22:04 +0700

    UI Login Done | UI Signup Done

--- resources/src/App.jsx
+++ resources/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Tes from "./pages/Tes";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import withLayout from "./lib/withLayout";
 import MainLayout from "./components/static/MainLayout";
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
                 <Route path="/" element={<HomeWithLayout />} />
                 <Route path="/tes" element={<TesWithLayout/>} /> 
                 <Route path="/login" element={<Login/>} /> 
+                <Route path="/signup" element={<Signup/>} /> 
             </Routes>
         </>
     );
--- resources/src/pages/Signup.jsx
+++ resources/src/pages/Signup.jsx
@@ -9,31 +9,41 @@ import { useEffect, useState } from "react";
 import { useFetch } from "@/lib/useFetch";
 
 const Home = () => {
+
     return (
         <>
             <div className="flex h-screen">
                 <div className="w-96 m-auto">
                     <title>Login</title>
-                    <LoginCard />
+                        <SignupCard />
                 </div>
             </div>
         </>
     );
 };
 
-const LoginCard = () => {
+const SignupCard = () => {
     return (
         <Card className="px-9 py-11 text-green-600">
             <div className="grid grid-cols-1 justify-items-center gap-2">
                 <LogIn className="size-10" />
 
-                <h1 className="text-3xl font-semibold">Login</h1>
+                <h1 className="text-3xl font-semibold">Sign Up</h1>
                 <p className="text-center">
-                    Login untuk bisa menggunakan layanan kami.
+                    Daftar sekarang untuk mulai menggunakan semua fitur kami.
                 </p>
             </div>
             <form className="">
                 <div className="grid grid-cols-1 gap-3">
+                    <div className="grid grid-cols-1 gap-1">
+                        <Label className="text-black font-normal">Nama</Label>
+                        <Input
+                            name="nama"
+                            className="border-[1.5px] bg-slate-200 rounded-sm text-black"
+                            placeholder="Masukkan Nama"
+                        />
+                        <ErrorAqil/>
+                    </div>
                     <div className="grid grid-cols-1 gap-1">
                         <Label className="text-black font-normal">Email</Label>
                         <Input
@@ -41,6 +51,7 @@ const LoginCard = () => {
                             className="border-[1.5px] bg-slate-200 rounded-sm text-black"
                             placeholder="example@company.com"
                         />
+                        <ErrorEmail/>
                     </div>
                     <div className="grid grid-cols-1 gap-1">
                         <Label className="text-black font-normal">
@@ -50,18 +61,29 @@ const LoginCard = () => {
                             type="password"
                             name="password"
                             className="border-[1.5px] bg-slate-200 rounded-sm text-black"
-                            placeholder="Masukkan password"
+                            placeholder="Gunakan password yang kuat"
                         />
-                        <ErrorDataKredensial/>
                         <ErrorPassword/>
+                    </div>
+                    <div className="grid grid-cols-1 gap-1">
+                        <Label className="text-black font-normal">
+                            Confirm Password
+                        </Label>
+                        <Input
+                            type="password"
+                            name="password"
+                            className="border-[1.5px] bg-slate-200 rounded-sm text-black"
+                            placeholder="Masukkan Kembali Password"
+                        />
+                        <ErrorConfirmPassword/>
                     </div>
                     <Button className="cursor pointer mt-5 bg-green-600 hover:bg-green-800">
                         Login
                     </Button>
                     <p className="text-gray-400 mx-auto text-[15px]">
-                        Belum punya akun?{" "}
-                        <a className="text-green-600" href="/signup">
-                            Sign Up
+                        Sudah punya akun?{" "}
+                        <a className="text-green-600" href="/login">
+                            Log In
                         </a>
                     </p>
                 </div>
@@ -70,10 +92,20 @@ const LoginCard = () => {
     );
 };
 
-const ErrorDataKredensial = () => {
+// Buat Pesan Error, pake yang dibutuhin aja cu  - Nabhan
+
+const ErrorConfirmPassword = () => {
     return (
         <p className="text-red-600 text-[13px]">
-            *Data kredensial yang anda masukkan tidak valid.
+            *Konfirmasi password tidak sesuai.
+        </p>
+    );
+};
+
+const ErrorEmail = () => {
+    return (
+        <p className="text-red-600 text-[13px]">
+            *Email sudah terpakai.
         </p>
     );
 };
@@ -81,9 +113,18 @@ const ErrorDataKredensial = () => {
 const ErrorPassword = () => {
     return (
         <p className="text-red-600 text-[13px]">
-            *Password yang anda masukkan salah.
+            *Password minimal memiliki 6 karakter.
         </p>
     );
 };
+
+const ErrorAqil = () => {
+    return (
+        <p className="text-red-600 text-[13px]">
+            *Aqil Butut.
+        </p>
+    );
+};
+
 
 export default Home;
